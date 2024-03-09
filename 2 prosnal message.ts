a2db003118dc9785b510a9771e528d9748855da5
Muzammil wahid <160735299+Muzammilwahid@users.noreply.github.com>
2024-03-09 20:28:18 +0500

    Add files via upload

---
+++ 2 prosnal message.ts
@@ -0,0 +1,14 @@
+
+//Store a person’s name in a variable, and print a message to that person. 
+//Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let personName :string = '';
+
+personName  = prompt('what is yor name?') !!;
+
+if(personName !== null && personName != ''){
+    alert(`“Hello ${personName}, would you like to learn some Python today?`)
+}
+else{
+    alert('you have to fill your name !')
+}
