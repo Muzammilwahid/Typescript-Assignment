83403064ba48dcac99f8c9699f669ce13d28ef55
Muzammil wahid <160735299+Muzammilwahid@users.noreply.github.com>
2024-03-20 01:25:38 +0500

    Add files via upload

---
+++ 17 Guest_List.ts
@@ -0,0 +1,43 @@
+
+
+let Guest_List :string[] =['salman' ,'ayaz' ,'haris '];
+   // for (let i = 0; i < Guest_List.length; i++) {
+   // console.log('Dear Mr. ' + Guest_List[i] + ', \n\n it is our pleasure to invite you in our party.\n\nThank You!\n\n ');
+ 
+   // }   
+  let absent_Guest :string ='ayaz';
+    let new_Guest :string ='Muzammil';  
+    Guest_List[1] =  new_Guest;
+
+    //for(let i=0; i<Guest_List.length; i++){
+     //  console.log('Dear Mr. ' + Guest_List[i] + ', \n\n it is our pleasure to invite you in our party.\n\nThank You!\n\n ')
+  //}
+    console.log(`Mr. ${absent_Guest} is not coming to the party `) 
+    console.log('Good news! we find big table so we are inviting 3 more guest. ')
+
+    //array mein 3 guest add kary hein 
+    Guest_List.unshift('Riwan');
+    Guest_List.splice(2 , 0 , 'Qasim');
+    Guest_List.push('fareed')
+     
+    //yahan mein ne 6 array ko priont karwaya ha 
+    for(let i=0; i<Guest_List.length; i++){
+    console.log('Dear Mr. ' + Guest_List[i] + ', \n\n it is our pleasure to invite you in our party.\n\nThank You!\n\n ')
+    }
+    //soory message to guest for not inviting 
+    console.log('\nSorry we can not arrange big table, only two people will be  invite');
+
+    //yahan par mein ne guest remove kary hein 
+    while(Guest_List.length > 2){
+       let remove_Guest = Guest_List.pop();
+       console.log(`Sorry Mr. ${remove_Guest}, Sorry sir you are not invite for dinner. `);
+    }
+
+    // hamary bachy hoy 2 invited guest
+    for(let i=0; i<Guest_List.length; i++){
+        console.log('Dear Mr. ' + Guest_List[i] + ', \n\n You are Still invited .\n\nThank You!\n\n ')
+       }
+ 
+       //mein ne apne sary guest remove kar deye
+    Guest_List.splice(0 , 2)
+    console.log(Guest_List);
